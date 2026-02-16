b8bf0468d3223ad2461c39d94ab9b35015bf2e45
Vitor Lopes <fvitorlopes@gmail.com>
2026-02-16 14:19:32 -0300

    First version, from the initializaer

---
+++ src/main/java/com/splash/task/SplashTaskApplication.java
@@ -0,0 +1,13 @@
+package com.splash.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplashTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplashTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/splash/task/SplashTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splash.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplashTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
